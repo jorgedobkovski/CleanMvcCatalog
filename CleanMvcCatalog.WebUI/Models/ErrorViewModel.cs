b654974d95dd668218ea1bc00130e2f5bac91b7f
Jorge Luiz Dourado Dobkovski <dobkovskijorge@gmail.com>
2025-12-01 19:45:59 -0400

    Adicionar arquivos de projeto.

---
+++ CleanMvcCatalog.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanMvcCatalog.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
