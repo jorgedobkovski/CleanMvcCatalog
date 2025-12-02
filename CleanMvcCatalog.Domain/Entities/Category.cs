9de42452d2fd053d1b2c281278dac5eaad518190
Jorge Luiz Dourado Dobkovski <dobkovskijorge@gmail.com>
2025-12-01 20:35:17 -0400

    feat: enrich domain model

--- CleanMvcCatalog.Domain/Entities/Category.cs
+++ CleanMvcCatalog.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanMvcCatalog.Domain.Validations;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +8,33 @@ namespace CleanMvcCatalog.Domain.Entities
     public sealed class Category : Entity
     {
         public string Name { get; private set; }
+        public ICollection<Product> Products { get; set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0,
+                "Invalid Id. Id must be greater than zero.");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required");
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name. Name is too short, minimum 3 characters");
+            Name = name;
+        }
     }
 }
--- CleanMvcCatalog.Domain/Entities/Entity.cs
+++ CleanMvcCatalog.Domain/Entities/Entity.cs
@@ -7,5 +7,7 @@ namespace CleanMvcCatalog.Domain.Entities
     public abstract class Entity
     {
         public int Id { get; protected set; }
+        public DateTime CreatedDate { get; protected set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; protected set; }
     }
 }
