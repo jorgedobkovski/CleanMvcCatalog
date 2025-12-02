a83452856716837c20549364ce2afb2ca97348df
Jorge Luiz Dourado Dobkovski <dobkovskijorge@gmail.com>
2025-12-01 20:46:55 -0400

    feat: add repository interfaces

---
+++ CleanMvcCatalog.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanMvcCatalog.Domain.Entities;
+
+namespace CleanMvcCatalog.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCatories();
+        Task<Category> GetById(int? id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
