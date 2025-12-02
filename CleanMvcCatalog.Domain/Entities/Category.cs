12d6ebcaf84964fc3052f1245c8dce6fc0cc58ad
Jorge Luiz Dourado Dobkovski <dobkovskijorge@gmail.com>
2025-12-01 20:14:23 -0400

    feat: add initial entities

---
+++ CleanMvcCatalog.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanMvcCatalog.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+    }
+}
---
+++ CleanMvcCatalog.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanMvcCatalog.Domain.Entities
+{
+    public sealed class Product : Entity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+    }
+}
