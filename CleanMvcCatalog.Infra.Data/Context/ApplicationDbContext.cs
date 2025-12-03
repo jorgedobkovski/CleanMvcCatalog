14595ae23547ea09d3b8acfa07614137cecf2a0b
Jorge Luiz Dourado Dobkovski <dobkovskijorge@gmail.com>
2025-12-02 23:30:07 -0400

    feat: add database context

---
+++ CleanMvcCatalog.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CleanMvcCatalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanMvcCatalog.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {       
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
