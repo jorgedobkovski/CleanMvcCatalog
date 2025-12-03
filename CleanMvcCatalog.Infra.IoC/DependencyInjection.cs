f7052c71270306842f34fa4077b19fd0383c6c4f
Jorge Luiz Dourado Dobkovski <dobkovskijorge@gmail.com>
2025-12-02 23:41:54 -0400

    feat: configurando injeção de dependencia

---
+++ CleanMvcCatalog.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanMvcCatalog.Domain.Interfaces;
+using CleanMvcCatalog.Infra.Data.Context;
+using CleanMvcCatalog.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanMvcCatalog.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+            ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
