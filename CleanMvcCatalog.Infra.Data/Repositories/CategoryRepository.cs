9e30f865e4ab932f2547d2422c211df7b0da7a6e
Jorge Luiz Dourado Dobkovski <dobkovskijorge@gmail.com>
2025-12-02 23:30:19 -0400

    feat: add repositories

---
+++ CleanMvcCatalog.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,55 @@
+﻿using CleanMvcCatalog.Domain.Entities;
+using CleanMvcCatalog.Domain.Interfaces;
+using CleanMvcCatalog.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanMvcCatalog.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private ApplicationDbContext _categoryContext;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _categoryContext = context;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetById(int? id)
+        {
+            return await _categoryContext.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _categoryContext.Categories.ToListAsync();
+        }
+
+        public Task<IEnumerable<Category>> GetCatories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            _categoryContext.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            _categoryContext.Update(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
