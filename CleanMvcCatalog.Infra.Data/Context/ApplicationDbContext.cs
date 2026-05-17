67ecb6b2de11b4a0e070365e6afb831779802719
Jorge Luiz Dourado Dobkovski <dobkovskijorge@gmail.com>
2026-05-17 15:17:55 -0400

    feat: setting new identity contextdb

--- CleanMvcCatalog.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanMvcCatalog.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using CleanMvcCatalog.Domain.Entities;
+using CleanMvcCatalog.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,7 +8,7 @@ using System.Text;
 
 namespace CleanMvcCatalog.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {       
---
+++ CleanMvcCatalog.Infra.Data/Identity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanMvcCatalog.Infra.Data.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
